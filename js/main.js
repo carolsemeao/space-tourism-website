e45c59820a585a0609f7e99b34fa966a4838631f
carolsemeao <carolina.semeao@outlook.com>
2023-03-29 21:10:28 +0200

    added accessibility and animations to index

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-// Navigation management on Mobile
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
+// When someone clicks the hamburger button
 navToggle.addEventListener("click", () => {
-    
-    const visiblity = nav.getAttribute("data-visible");
-    if (visiblity === "false") {
+    const visibility = nav.getAttribute("data-visible");
+    if (visibility == "false") {
+        // if the nav is closed, open it
         nav.setAttribute("data-visible", true);
         navToggle.setAttribute("aria-expanded", true);
     } else {
+        // if the nav is open, close it
         nav.setAttribute("data-visible", false);
         navToggle.setAttribute("aria-expanded", false);
     }
